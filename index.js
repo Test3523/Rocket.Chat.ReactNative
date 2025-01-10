947b1ce2da0697a2665b76f0d9da1a274b3c4e30
Rohit3523 <Rohit3523@users.noreply.github.com>
2025-01-10 16:46:58 +0000

    chore: format code with Prettier

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ import 'react-native-gesture-handler';
 import 'react-native-console-time-polyfill';
 import { AppRegistry } from 'react-native';
 
-import {
-	name as appName,
-	share as shareName
-} from './app.json';
+import { name as appName, share as shareName } from './app.json';
 import { isFDroidBuild } from './app/lib/constants';
 import { isAndroid } from './app/lib/methods/helpers';
 
