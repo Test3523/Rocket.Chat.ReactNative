57f707a2ac47e1a15f91e02634ab66428dddd09f
Rohit Bansal <40559587+Rohit3523@users.noreply.github.com>
2025-03-12 23:45:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import 'react-native-gesture-handler';
 import 'react-native-console-time-polyfill';
-import { AppRegistry } from 'react-native';
 
 import { name as appName, share as shareName } from './app.json';
 import { isFDroidBuild } from './app/lib/constants';
+import { AppRegistry } from 'react-native';
 import { isAndroid } from './app/lib/methods/helpers';
 
 if (__DEV__) {
-	require('./app/ReactotronConfig');
+	require('../app/ReactotronConfig');
 } else {
 	console.log = () => {};
 	console.time = () => {};
