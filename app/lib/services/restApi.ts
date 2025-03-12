773232e8d0efcc1b0092c54b1c7872e5dfa58800
Rohit Bansal <40559587+Rohit3523@users.noreply.github.com>
2025-03-12 20:12:56 +0530

    Update restApi.ts

--- app/lib/services/restApi.ts
+++ app/lib/services/restApi.ts
@@ -308,9 +308,10 @@ export const setUserPreferences = (userId: string, data: Partial<INotificationPr
 	// RC 0.62.0
 	sdk.post('users.setPreferences', { userId, data });
 
-export const setUserStatus = (status: string, message: string) =>
+export const setUserStatus = (status: string, message: string) => {
 	// RC 1.2.0
-	sdk.methodCall('setUserStatus', status, message);
+	return sdk.methodCall('setUserStatus', status, message);
+}
 
 export const setReaction = (emoji: string, messageId: string) =>
 	// RC 0.62.2
